9043114ecaed89026db9abb4bb345964fb3c1748
Agostina Capponi <capponi.agostina@gmail.com>
2020-09-05 18:21:42 -0300

    Updates

---
+++ precache-manifest.284d752825138fcce7bf9b4acd540fdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe604d69770ddf9d8deac9347157938",
+    "url": "/tourabu-expedition-calculator/index.html"
+  },
+  {
+    "revision": "e6fd51519a246db6c7d4",
+    "url": "/tourabu-expedition-calculator/static/css/main.5272aa1a.chunk.css"
+  },
+  {
+    "revision": "473c25f6c5469c3e806a",
+    "url": "/tourabu-expedition-calculator/static/js/2.769d727e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tourabu-expedition-calculator/static/js/2.769d727e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fd51519a246db6c7d4",
+    "url": "/tourabu-expedition-calculator/static/js/main.1aba2e92.chunk.js"
+  },
+  {
+    "revision": "b3b7090ac1b1f79d708b",
+    "url": "/tourabu-expedition-calculator/static/js/runtime-main.af8f890b.js"
+  },
+  {
+    "revision": "95fe5256d68243a9ca96fe7c4127fcd3",
+    "url": "/tourabu-expedition-calculator/static/media/j-lee-LYBSBjGo-5s-unsplash.95fe5256.jpg"
+  }
+]);
